919290b2ce9a3c45cc77755f9c48d4de1cb554a1
William Green <kulu1kulu2@gmail.com>
2017-07-06 12:45:52 +0000

    Add username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+  
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+         
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+  
   has_many :posts
 end
