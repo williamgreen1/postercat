5a5b392824569809ce51ed787c6aa62a1242f209
William Green <kulu1kulu2@gmail.com>
2017-07-06 09:41:58 +0000

    Add posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705130009) do
+ActiveRecord::Schema.define(version: 20170706084132) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
