df9301a03116ba56f873a0ae79c88ac743956681
William Green <kulu1kulu2@gmail.com>
2017-07-07 12:43:57 +0000

    final push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  
   resources :posts
-  devise_for :users
+  
+  resources :users
+  
+  devise_for :users,
+  			:path => '',
+  			:path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+  
+  
   root 'posts#index'
 end
